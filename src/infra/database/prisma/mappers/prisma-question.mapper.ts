9dd9cb23eefccb00c7fce080653f4a508f7da5ea
Guilherme Gonçalves <gui.denez56@gmail.com>
2025-07-14 08:19:34 -0300

    Add: prisma questions repository

--- src/infra/database/prisma/mappers/prisma-question.mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question.mapper.ts
@@ -1,7 +1,8 @@
+import { Question as PrismaQuestion, Prisma } from 'generated/prisma';
+
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { Question } from '@/domain/forum/enterprise/entities/question';
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug';
-import { Question as PrismaQuestion } from 'generated/prisma';
 
 export class PrismaQuestionMapper {
   static toDomain(raw: PrismaQuestion): Question {
@@ -10,12 +11,27 @@ export class PrismaQuestionMapper {
         title: raw.title,
         content: raw.content,
         authorId: new UniqueEntityID(raw.authorId),
-        bestAnswerId: undefined,
+        bestAnswerId: raw.bestAnswerId
+          ? new UniqueEntityID(raw.bestAnswerId)
+          : null,
         slug: Slug.create(raw.slug),
         createdAt: raw.createdAt,
         updatedAt: raw.updatedAt,
       },
       new UniqueEntityID(raw.id),
     );
+  }
+
+  static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+    return {
+      id: question.id.toString(),
+      authorId: question.authorId.toString(),
+      bestAnswerId: question.bestAnswerId?.toString(),
+      title: question.title,
+      content: question.content,
+      slug: question.slug.value,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    };
   }
 }
