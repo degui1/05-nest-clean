86301dff196e59c14cc4f2dfb6757b5bab83608e
Guilherme Gonçalves <gui.denez56@gmail.com>
2025-07-14 07:51:27 -0300

    Add: prisma repositories

---
+++ src/infra/database/prisma/repositories/prisma-questions.repository.ts
@@ -0,0 +1,36 @@
+import { PaginationParams } from '@/core/repositories/pagination-params';
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository';
+import { Question } from '@/domain/forum/enterprise/entities/question';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { PrismaQuestionMapper } from '../mappers/prisma-question.mapper';
+
+@Injectable()
+export class PrismaQuestionsRepository implements QuestionsRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findById(id: string) {
+    const question = await this.prisma.question.findUnique({ where: { id } });
+
+    if (!question) {
+      return null;
+    }
+
+    return PrismaQuestionMapper.toDomain(question);
+  }
+  findBySlug(slug: string) {
+    throw new Error('Method not implemented.');
+  }
+  findManyRecent(params: PaginationParams) {
+    throw new Error('Method not implemented.');
+  }
+  save(question: Question) {
+    throw new Error('Method not implemented.');
+  }
+  create(question: Question) {
+    throw new Error('Method not implemented.');
+  }
+  delete(question: Question) {
+    throw new Error('Method not implemented.');
+  }
+}
