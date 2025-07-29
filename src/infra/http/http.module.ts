a091df86cf814920f046d4d8acccca69a2124de2
Guilherme Gonçalves <gui.denez56@gmail.com>
2025-07-29 08:11:39 -0300

    Add: jwt auth guard

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question';
+import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student';
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions';
 
-import { DatabaseModule } from '../database/database.module';
-import { CreateAccountController } from './controllers/create-account.controller';
-import { AuthenticateController } from './controllers/authenticate.controller';
-import { CreateQuestionController } from './controllers/create-question.controller';
-import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student';
 import { CryptographyModule } from '../cryptography/Cryptography.module';
+import { DatabaseModule } from '../database/database.module';
+import { AuthenticateController } from './controllers/authenticate.controller';
+import { CreateAccountController } from './controllers/create-account.controller';
+import { CreateQuestionController } from './controllers/create-question.controller';
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
