998bce4350a543903f6058c490116316b31eb672
Guilherme Gonçalves <gui.denez56@gmail.com>
2025-08-02 18:16:42 -0300

    Add: Fetch question answers controller

--- src/domain/forum/application/use-cases/fetch-question-answers.ts
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -1,4 +1,7 @@
+import { Injectable } from '@nestjs/common';
+
 import { Either, right } from '@/core/either';
+
 import { Answer } from '../../enterprise/entities/answer';
 import { AnswersRepository } from '../repositories/answers-repository';
 
@@ -14,6 +17,7 @@ type FetchQuestionAnswersUseCaseResponse = Either<
   }
 >;
 
+@Injectable()
 export class FetchQuestionAnswersUseCase {
   constructor(private answersRepository: AnswersRepository) {}
 
