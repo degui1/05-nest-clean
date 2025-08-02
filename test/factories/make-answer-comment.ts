ecdfd499edd87f3c355bebe21fffe579825ffdf2
Guilherme Gonçalves <gui.denez56@gmail.com>
2025-08-02 12:19:24 -0300

    Add: answer, answer comment and question comment factory

--- test/factories/make-answer-comment.ts
+++ test/factories/make-answer-comment.ts
@@ -1,3 +1,4 @@
+import { Injectable } from '@nestjs/common';
 import { faker } from '@faker-js/faker';
 
 import {
@@ -5,6 +6,8 @@ import {
   AnswerCommentProps,
 } from '@/domain/forum/enterprise/entities/answer-comment';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { PrismaAnswerCommentMapper } from '@/infra/database/prisma/mappers/prisma-answer-comment.mapper';
 
 export function makeAnswerComment(
   override: Partial<AnswerCommentProps> = {},
@@ -21,4 +24,21 @@ export function makeAnswerComment(
   );
 
   return answerComment;
+}
+
+@Injectable()
+export class AnswerCommentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaAnswerComment(
+    data: Partial<AnswerCommentProps> = {},
+  ): Promise<AnswerComment> {
+    const answerComment = makeAnswerComment(data);
+
+    await this.prisma.comment.create({
+      data: PrismaAnswerCommentMapper.toPrisma(answerComment),
+    });
+
+    return answerComment;
+  }
 }
